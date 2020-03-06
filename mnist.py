4a576988abc43fba2cf1697a82c77436f21ad171
Akitaka Wakasugi <akitaka@AkitakanoMacBook-Pro.local>
2020-03-06 13:24:13 +0900

    data augumentation shift width 3

--- mnist.py
+++ mnist.py
@@ -1,19 +1,21 @@
 from sklearn import datasets, model_selection, svm, metrics
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from PIL import Image
 import numpy as np
 import itertools
 np.random.seed(42)
 
 
-def augument_data(len_shift, data_list):
+def augument_data(len_shift, data_list, data_width):
     shift_length = range(len_shift)
-    augumented_data_width = 24
     shift_position = list(itertools.product(shift_length, shift_length))
-    return np.array([[data[y: y + augumented_data_width, x: x + augumented_data_width] for y, x in shift_position] for data in data_list])
+    return np.array([data[y: y + data_width, x: x + data_width] for data in data_list for y, x in shift_position])
 
 def augument_label(len_shift, label):
-    num_repeat = len_shift * len_shift
-    return np.repeat(label, num_repeat)
+    repeat_times = len_shift * len_shift
+    return np.repeat(label, repeat_times)
+    # num_repeat = len_shift * len_shift
+    # return [num_repeat for _i in range(9)]
 
 def whitening(X_train, X_test, epsilon = 1e-5):
     mean_x = np.mean(X_train)
@@ -33,8 +35,7 @@ def calculate_log_likelihood_each_class(input_data, inverse_sigma, mu_each_class
     return np.dot(np.dot((input_data.T - mu_each_class.T) / 2, inverse_sigma), mu_each_class) + np.log(num_samples_eachclass)
 
 def predict_label(data, inverse_sigma, mu_each_class_list, num_train_sample_per_class, num_class):
-    log_likelihood_each_class = [calculate_log_likelihood_each_class(data, inverse_sigma, mu_each_class_list[i], num_train_sample_per_class[i]) for i in range(num_class)]
-    return str(np.argmax(log_likelihood_each_class))
+    return str(np.argmax([calculate_log_likelihood_each_class(data, inverse_sigma, mu_each_class_list[i], num_train_sample_per_class[i]) for i in range(num_class)]))
 
 def make_predicted_label_list(test_data, inverse_sigma, mu_each_class_list, num_train_sample_per_class, num_class):
     return np.array([predict_label(data, inverse_sigma, mu_each_class_list, num_train_sample_per_class, num_class) for data in test_data])
@@ -51,36 +52,27 @@ def main():
     num_train_sample = train_data.shape[0]
     num_test_sample = test_data.shape[0]
 
-    data_width, shift_width = 28, 2
+    data_width, shift_width = 28, 1
     shift_length = shift_width * 2 + 1
-    train_data = train_data.reshape(num_train_data, data_width, data_width)
-    test_data = test_data.reshape(num_test_data, data_width, data_width)
-
-    train_data = augument_data(shift_length, train_data)
-    label_train = augument_label(shift_length, label_train)
+    train_data = train_data.reshape(num_train_sample, data_width, data_width)
+    test_data = test_data.reshape(num_test_sample, data_width, data_width)
     test_data = np.array([data[shift_width: data_width - shift_width, shift_width: data_width - shift_width] for data in test_data])
 
-    data_width, num_train_sample = 24, num_train_data * shift_length * shift_length
-    train_data = train_data.reshape(num_train_data, data_width * data_width)
-    test_data = test_data.reshape(num_test_data, data_width * data_width)
+    data_width = 26
+    train_data = augument_data(shift_length, train_data, data_width)
+    label_train = augument_label(shift_length, label_train)
+
+    num_train_sample = num_train_sample * shift_length * shift_length
+    train_data = train_data.reshape(num_train_sample, data_width * data_width)
+    test_data = test_data.reshape(num_test_sample, data_width * data_width)
 
     train_data, test_data = whitening(train_data, test_data)
     print('finish whitening')
     num_train_sample_per_class = np.array([np.count_nonzero(label_train == str(i)) for i in range(num_class)])
-    train_data_eachclass_list = np.array([train_data[label_train == str(i)] for i in range(num_class)])
 
-    # sum_sigma = 0
-    # cov_eachclass_list = [np.cov(data_list.T) for data_list in train_data_eachclass_list]
-    # for sigma, num_samples in zip(cov_eachclass_list, num_train_sample_per_class):
-    #     sum_sigma += sigma * num_samples
-    # print('sum_sigma:', sum_sigma)
-    # sigma = sum_sigma / num_train_sample
-    # ↑各カテゴリで分散共分散行列を計算
-    # ↓カテゴリに関係なく一斉に分散共分散行列を計算
     sigma = np.cov(train_data.T)
 
     inverse_sigma = np.linalg.pinv(sigma)
-    print('inverse_sigma:', inverse_sigma)
 
     predicted_label = make_predicted_label_list(test_data, inverse_sigma, np.array([np.mean(train_data[label_train == str(i)], axis = 0) for i in range(num_class)]), 
     num_train_sample_per_class, num_class)
